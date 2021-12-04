26ab011b3ec818d1f168b7c1448f9c557571085e
WanderROS <wsp1194409532@gmail.com>
2021-12-04 17:49:08 +0800

    markdowb

--- StepByStep/ContentView.swift
+++ StepByStep/ContentView.swift
@@ -7,10 +7,52 @@
 //
 
 import SwiftUI
+import MDText
+import Parma
+
 
 struct ContentView: View {
+    @State var showingPopup = true
+    static let string =
+        """
+    # hello, This is Markdown Live Preview
+    ----
+    ## what is Markdown?
+    see [Wikipedia](https://en.wikipedia.org/wiki/Markdown)
+    > Markdown is a lightweight markup language, originally created by John Gruber and Aaron Swartz allowing people "to write using an easy-to-read, easy-to-write plain text format, then convert it to structurally valid XHTML (or HTML)".
+    ----
+    ## usage
+    1. Write markdown text in this textarea.
+    2. Click 'HTML Preview' button.
+    ----
+    ## markdown quick reference
+    # headers
+    *emphasis*
+    **strong**
+    * list
+    >block quote
+        code (4 spaces indent)
+    [links](https://wikipedia.org)
+    ----
+    ## changelog
+    * 17-Feb-2013 re-design
+    ----
+    ## thanks
+    * [markdown-js](https://github.com/evilstreak/markdown-js)
+    """
     var body: some View {
-        Text("Hello, World!")
+        ZStack{
+            VStack{
+                MDText(markdown: ContentView.string)
+                Button(action: {
+                    self.showingPopup = true
+                }, label: {
+                    Text("按钮")
+                })
+            }
+        }
+    
+
     }
 }
 
