24f1b56de30556bf7ea02088a504d2473faf764e
WanderROS <wsp1194409532@gmail.com>
2021-12-05 16:15:33 +0800

    swiftUI 文本

--- StepByStep/ContentView.swift
+++ StepByStep/ContentView.swift
@@ -9,8 +9,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var secured = false
+    @State var password = ""
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        VStack {
+            HStack {
+                Text("密码：").frame(width: 50, alignment: .leading)
+                    .padding(.leading,30)
+                if self.secured
+                { TextField("Password",text: self.$password).padding(.horizontal,5)
+                    .frame(width: UIScreen.main.bounds.width - 80,height: 50)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .background(Color.white)
+                    .cornerRadius(25)
+                    
+                } else {
+                    SecureField("Password",text: self.$password).padding(.horizontal,5)
+                        .frame(width: UIScreen.main.bounds.width - 80,height: 50)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .background(Color.white)
+                        .cornerRadius(25)
+                    
+                }
+                Button(action: {self.secured.toggle()}) {
+                    Image(systemName: self.secured ? "eye.fill": "eye.slash.fill")
+                        .frame(width: 34, height: 34, alignment: .center)
+                        .foregroundColor((self.secured == true) ? Color.green : Color.secondary)
+                } .offset(x: -50
+                    , y: 0)
+            }
+        }
     }
 }
 
