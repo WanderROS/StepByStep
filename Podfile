e3469888273ad0cbf45e0d374870e3c7f00c675d
WanderROS <wsp1194409532@gmail.com>
2021-12-05 19:40:41 +0800

    swiftUI alert

--- Podfile
+++ Podfile
@@ -4,6 +4,8 @@
 target 'StepByStep' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
+  
+  pod 'SPAlert'
 
   # Pods for StepByStep
 
--- StepByStep/ContentView.swift
+++ StepByStep/ContentView.swift
@@ -7,52 +7,23 @@
 //
 
 import SwiftUI
-
+import SPAlert
 struct ContentView : View {
-    @State var show = false
-    
+    @State var showAlert = false
     var body: some View {
-        VStack {
-            Text("Card in SwiftUI")
-                .foregroundColor(.white)
-                .fontWeight(.bold)
-                .font(.largeTitle)
-                .padding(.top, show ? 30 : 20)
-                .padding(.bottom, show ? 20 : 0)
-            
-            Text("Animatable cards with Spring, custom frame and some paddings. Also use SFSymbol for icon in the bottom button. Tap to button fo see fill style of this icon.")
-                .foregroundColor(.white)
-                .multilineTextAlignment(.center)
-                .animation(.spring())
-                .cornerRadius(0)
-                .lineLimit(.none)
-            
-            Spacer()
-            
-            Button(action: {
-                self.show.toggle()
-            }) {
-                HStack {
-                    Image(systemName: show ? "slash.circle.fill" : "slash.circle")
-                        .foregroundColor(Color(hue: 0.498, saturation: 0.609, brightness: 1.0))
-                        .font(Font.title.weight(.semibold))
-                        .imageScale(.small)
-                    Text(show ? "to Card" : "to Area")
-                        .foregroundColor(Color(hue: 0.498, saturation: 0.609, brightness: 1.0))
-                        .fontWeight(.bold)
-                        .font(.title)
-                        .cornerRadius(0)
-                }
-            }
-            .padding(.bottom, show ? 20 : 15)
-            
-        }
-        .padding()
-        .padding(.top, 15)
-        .frame(width: show ? 350 : 290, height: show ? 420 : 260)
-        .background(Color.blue)
-        .cornerRadius(30)
-        .animation(.spring())
+       Button("Show alert") {
+        self.showAlert = true
+       }.spAlert(isPresent: $showAlert,
+               title: "Alert title",
+               message: "Alert message",
+               duration: 2.0,
+               dismissOnTap: true,
+               preset: .custom(UIImage(systemName: "heart")!),
+               haptic: .success,
+               layout: .init(),
+               completion: {
+                   print("Alert is destory")
+               })
     }
 }
 struct ContentView_Previews: PreviewProvider {
