f8f4641da5090e2ab3d02bbdfeff4d9d5e741910
WanderROS <wsp1194409532@gmail.com>
2021-12-05 19:28:55 +0800

    swiftUI spring

--- StepByStep/ContentView.swift
+++ StepByStep/ContentView.swift
@@ -8,41 +8,53 @@
 
 import SwiftUI
 
-struct ContentView: View {
-  @State var webViewStore = WebViewStore()
-  
-  var body: some View {
-    NavigationView {
-      WebView(webView: webViewStore.webView)
-//        .navigationBarTitle(Text(verbatim: webViewStore.title ?? ""), displayMode: .inline)
-//        .navigationBarItems(trailing: HStack {
-//          Button(action: goBack) {
-//            Image(systemName: "chevron.left")
-//              .imageScale(.large)
-//              .aspectRatio(contentMode: .fit)
-//              .frame(width: 32, height: 32)
-//          }.disabled(!webViewStore.canGoBack)
-//          Button(action: goForward) {
-//            Image(systemName: "chevron.right")
-//              .imageScale(.large)
-//              .aspectRatio(contentMode: .fit)
-//              .frame(width: 32, height: 32)
-//          }.disabled(!webViewStore.canGoForward)
-//        })
-    }.onAppear {
-      self.webViewStore.webView.load(URLRequest(url: URL(string: "https://www.baidu.com")!))
+struct ContentView : View {
+    @State var show = false
+    
+    var body: some View {
+        VStack {
+            Text("Card in SwiftUI")
+                .foregroundColor(.white)
+                .fontWeight(.bold)
+                .font(.largeTitle)
+                .padding(.top, show ? 30 : 20)
+                .padding(.bottom, show ? 20 : 0)
+            
+            Text("Animatable cards with Spring, custom frame and some paddings. Also use SFSymbol for icon in the bottom button. Tap to button fo see fill style of this icon.")
+                .foregroundColor(.white)
+                .multilineTextAlignment(.center)
+                .animation(.spring())
+                .cornerRadius(0)
+                .lineLimit(.none)
+            
+            Spacer()
+            
+            Button(action: {
+                self.show.toggle()
+            }) {
+                HStack {
+                    Image(systemName: show ? "slash.circle.fill" : "slash.circle")
+                        .foregroundColor(Color(hue: 0.498, saturation: 0.609, brightness: 1.0))
+                        .font(Font.title.weight(.semibold))
+                        .imageScale(.small)
+                    Text(show ? "to Card" : "to Area")
+                        .foregroundColor(Color(hue: 0.498, saturation: 0.609, brightness: 1.0))
+                        .fontWeight(.bold)
+                        .font(.title)
+                        .cornerRadius(0)
+                }
+            }
+            .padding(.bottom, show ? 20 : 15)
+            
+        }
+        .padding()
+        .padding(.top, 15)
+        .frame(width: show ? 350 : 290, height: show ? 420 : 260)
+        .background(Color.blue)
+        .cornerRadius(30)
+        .animation(.spring())
     }
-  }
-  
-  func goBack() {
-    webViewStore.webView.goBack()
-  }
-  
-  func goForward() {
-    webViewStore.webView.goForward()
-  }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
