57c5062d8bced604f3f85e34dec7c39124ecea05
WanderROS <wsp1194409532@gmail.com>
2021-12-05 19:17:13 +0800

    swiftUI webview

--- StepByStep/ContentView.swift
+++ StepByStep/ContentView.swift
@@ -9,23 +9,38 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State var secured = false
-    @State var password = ""
-    
-    var body: some View {
-        
-        VStack {
-            NavigationView {
-                    NavigationLink(destination: EditButtonPage()) {
-                        Text("NavigationButton").bold()
-                            .foregroundColor(.orange)
-                            .font(.largeTitle)
-                        }
-                .navigationBarTitle(Text("Page"))
-            }
-        }
+  @State var webViewStore = WebViewStore()
+  
+  var body: some View {
+    NavigationView {
+      WebView(webView: webViewStore.webView)
+//        .navigationBarTitle(Text(verbatim: webViewStore.title ?? ""), displayMode: .inline)
+//        .navigationBarItems(trailing: HStack {
+//          Button(action: goBack) {
+//            Image(systemName: "chevron.left")
+//              .imageScale(.large)
+//              .aspectRatio(contentMode: .fit)
+//              .frame(width: 32, height: 32)
+//          }.disabled(!webViewStore.canGoBack)
+//          Button(action: goForward) {
+//            Image(systemName: "chevron.right")
+//              .imageScale(.large)
+//              .aspectRatio(contentMode: .fit)
+//              .frame(width: 32, height: 32)
+//          }.disabled(!webViewStore.canGoForward)
+//        })
+    }.onAppear {
+      self.webViewStore.webView.load(URLRequest(url: URL(string: "https://www.baidu.com")!))
     }
+  }
+  
+  func goBack() {
+    webViewStore.webView.goBack()
+  }
+  
+  func goForward() {
+    webViewStore.webView.goForward()
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
